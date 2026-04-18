11fdca75c1100cb2615c945fc5dc40a60a63bcea
Filip Trivan <filiptrivan@github.com>
2026-04-18 09:00:19 +0000

    Update Spiderly star count

--- src/components/navigation/mobile-navbar.tsx
+++ src/components/navigation/mobile-navbar.tsx
@@ -87,7 +87,7 @@ const MobileNavbar = () => {
                 <Star className="size-4 mr-2" />
                 Star on GitHub
                 <span className="ml-2 px-2 py-0.5 bg-muted rounded-full text-xs font-semibold">
-                  50
+                  51
                 </span>
               </Link>
             </div>
